709998ed6b917eb00b03a001d2dd6d99b7667bfd
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-12-12 17:34:02 +0200

    Statistic Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,23 @@
 import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
 import user from '../user.json';
-
-console.log(user);
+import data from '../data.json';
 
 export const App = () => {
   return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        followers={user.stats.followers}
-        views={user.stats.views}
-        likes={user.stats.likes}
-      />
-    </div>
+    <>
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          followers={user.stats.followers}
+          views={user.stats.views}
+          likes={user.stats.likes}
+        />
+      </div>
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { StatsList } from 'components/Statistics/StatsList';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {{ title } && <h2 className={css.title}>{title.toUpperCase()}</h2>}
+      <ul className={css.stat_list}>
+        {stats.map(stat => (
+          <StatsList
+            label={stat.label}
+            percentage={stat.percentage}
+            key={stat.id}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
+};
