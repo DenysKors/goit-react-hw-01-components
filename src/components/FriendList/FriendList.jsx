a7f71e3b1d3e288cc71122b207a1b7296f1558d5
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-12-13 12:04:06 +0200

    FriendList Component

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from 'components/FriendList/FriendListItem';
-// import css from './FriendList.module.css';
+import css from './FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={css.friend_list}>
       {friends.map(friend => (
         <FriendListItem
           avatar={friend.avatar}
